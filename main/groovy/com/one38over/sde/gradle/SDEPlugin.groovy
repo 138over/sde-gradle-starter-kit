deec1ad542bdac2f9d3def92844d9d1ee47909b9
138over <kramerica@icloud.com>
2022-09-21 11:59:49 -0700

    initial sde plugin skeleton

---
+++ main/groovy/com/one38over/sde/gradle/SDEPlugin.groovy
@@ -0,0 +1,9 @@
+package com.one38over.sde.gradle
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class SDEPlugin implements Plugin<Project>{
+    void apply(Project project) {
+    }
+}
